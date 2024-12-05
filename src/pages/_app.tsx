610ce323b8993f95f3c68ff6a5d7cb49d2c36580
r2ditto <ryan.ebro@yahoo.com>
2024-12-04 22:19:57 -0500

    fix chakra provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,10 @@
-import { Provider } from "@/components/ui/provider";
 import type { AppProps } from "next/app";
-
-import "@/styles/globals.css";
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider>
+    <ChakraProvider value={defaultSystem}>
       <Component {...pageProps} />
-    </Provider>
+    </ChakraProvider>
   );
 }
