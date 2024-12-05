b80299d479fb92955c216d53f482aa2c4db9fbb1
r2ditto <ryan.ebro@yahoo.com>
2024-12-04 19:37:45 -0500

    chakra ui set up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
+import { Provider } from "@/components/ui/provider";
 import type { AppProps } from "next/app";
 
+import "@/styles/globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
