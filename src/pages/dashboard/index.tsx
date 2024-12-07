33a79027d56cfc276e909c3de85b50a6a26907ba
r2ditto <ryan.ebro@yahoo.com>
2024-12-07 02:37:59 -0500

    Fetch and display resume

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useUser } from "@/contexts/UserContext";
+import { supabase } from "@/utils/supabase";
+
 import DashboardLayout from "@/components/layout/DashboardLayout";
 
+import ResumeList from "@/components/ui/resume-list";
+
 export default function Dashboard() {
+  const { user } = useUser();
+  const [resumes, setResumes] = useState<any[] | null>([]);
+
+  useEffect(() => {
+    if (!user?.id) return;
+
+    const fetchResumes = async () => {
+      try {
+        const { data } = await supabase
+          .from("resumes")
+          .select("*")
+          .eq("user_id", user?.id);
+
+        setResumes(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchResumes();
+  }, [user?.id]);
+
   return (
     <DashboardLayout>
-      <div>Dashboard</div>
+      <ResumeList resumes={resumes} />
     </DashboardLayout>
   );
 }
