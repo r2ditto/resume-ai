96119c9e436fbef111b108d2cb36d06f3ea2533b
r2ditto <ryan.ebro@yahoo.com>
2024-12-10 02:25:02 -0500

    use enum for defining resume sections

--- src/pages/resumes/[id].tsx
+++ src/pages/resumes/[id].tsx
@@ -17,18 +17,43 @@ import {
   TbStar,
   TbUserCircle,
 } from "react-icons/tb";
+import PersonalDetailsForm from "@/components/forms/personal-details-form";
 
+enum ResumeSections {
+  PersonalInformation = "Personal Information",
+  ContactInformation = "Contact Information",
+  ProfessionalSummary = "Professional Summary",
+  Links = "Links",
+  EmploymentHistory = "Employment History",
+  Education = "Education",
+  Certifications = "Certifications",
+  Skills = "Skills",
+}
+
+// Switching sections should only rerender sidebar and resume, not preview
 export default function ResumePage() {
-  const [items, setItems] = useState([
-    "Personal Information",
-    "Contact Information",
-    "Professional Summary",
-    "Links",
-    "Employment History",
-    "Education",
-    "Certifications",
-    "Skills",
+  const [items, setItems] = useState<ResumeSections[]>([
+    ResumeSections.PersonalInformation,
+    ResumeSections.ContactInformation,
+    ResumeSections.ProfessionalSummary,
+    ResumeSections.Links,
+    ResumeSections.EmploymentHistory,
+    ResumeSections.Education,
+    ResumeSections.Certifications,
+    ResumeSections.Skills,
   ]);
+  const [activeSection, setActiveSection] = useState<ResumeSections>(
+    ResumeSections.PersonalInformation
+  );
+
+  const renderForm = () => {
+    switch (activeSection) {
+      case ResumeSections.PersonalInformation:
+        return <PersonalDetailsForm />;
+      default:
+        return null;
+    }
+  };
 
   const itemIcons = {
     "Personal Information": <TbUserCircle size={24} />,
@@ -50,6 +75,9 @@ export default function ResumePage() {
 
     setItems(reorderedItems);
   };
+
+  const handleSectionClick = (section: ResumeSections) =>
+    setActiveSection(section);
 
   return (
     <Grid templateColumns="1fr 3fr 2fr" gap="5">
@@ -75,13 +103,15 @@ export default function ResumePage() {
                               backgroundColor: "gray.100",
                             }}
                             borderRadius="lg"
-                            p="2"
-                            my="2"
+                            px="2"
+                            py="3"
+                            my="1"
                             fontSize={16}
                             fontWeight={500}
                             ref={provided.innerRef}
                             {...provided.draggableProps}
                             {...provided.dragHandleProps}
+                            onClick={() => handleSectionClick(item)} // Added onClick event
                           >
                             <Flex gap="2" alignItems="center">
                               {itemIcons[item as keyof typeof itemIcons]}
@@ -104,7 +134,8 @@ export default function ResumePage() {
       <GridItem>
         <Card.Root variant="elevated">
           <Card.Body>
-            <Text>Resume</Text>
+            <Text>{activeSection}</Text>
+            {renderForm()}
           </Card.Body>
         </Card.Root>
       </GridItem>
