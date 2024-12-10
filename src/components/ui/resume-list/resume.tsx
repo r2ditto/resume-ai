2126ba1a2ce00536c7e2a2a6dfd707d3281cc941
r2ditto <ryan.ebro@yahoo.com>
2024-12-10 01:45:49 -0500

    add routing to resume page

--- src/components/ui/resume-list/resume.tsx
+++ src/components/ui/resume-list/resume.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useRouter } from "next/router";
+
 import { Card, Box, Heading, Text, Flex, VStack } from "@chakra-ui/react";
 
 import { TbEdit } from "react-icons/tb";
@@ -6,11 +8,16 @@ import { TbCopy } from "react-icons/tb";
 import { TbTrash } from "react-icons/tb";
 
 export default function Resume({ resume }: { resume: any }) {
+  const router = useRouter();
   const formattedDate = new Intl.DateTimeFormat("en-US", {
     day: "numeric",
     month: "short",
     year: "numeric",
   }).format(new Date(resume.updated_at));
+
+  const handleEdit = () => {
+    router.push(`/resumes/${resume.id}`);
+  };
 
   return (
     <Card.Root w="full" borderRadius="xl" bg="gray.100" border="none">
@@ -24,15 +31,34 @@ export default function Resume({ resume }: { resume: any }) {
           </Box>
 
           <VStack marginTop={12} gap={8} alignItems="flex-start">
-            <Flex fontSize="25px" alignItems="center" gap={2} fontWeight="500">
+            <Flex
+              as="button"
+              fontSize="25px"
+              alignItems="center"
+              gap={2}
+              fontWeight="500"
+              onClick={handleEdit}
+            >
               <TbEdit size="30px" /> Edit
             </Flex>
 
-            <Flex fontSize="25px" alignItems="center" gap={2} fontWeight="500">
+            <Flex
+              as="button"
+              fontSize="25px"
+              alignItems="center"
+              gap={2}
+              fontWeight="500"
+            >
               <TbCopy size="30px" /> Copy
             </Flex>
 
-            <Flex fontSize="25px" alignItems="center" gap={2} fontWeight="500">
+            <Flex
+              as="button"
+              fontSize="25px"
+              alignItems="center"
+              gap={2}
+              fontWeight="500"
+            >
               <TbTrash size="30px" /> Delete
             </Flex>
           </VStack>
