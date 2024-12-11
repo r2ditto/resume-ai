470d7e1e2026b752b839059ae9a5f7d6b78e7a47
r2ditto <ryan.ebro@yahoo.com>
2024-12-10 22:29:49 -0500

    add helper.ts file

---
+++ src/utils/helper.ts
@@ -0,0 +1,16 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number
+) {
+  let timeoutId: NodeJS.Timeout;
+
+  return (...args: Parameters<T>): void => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
